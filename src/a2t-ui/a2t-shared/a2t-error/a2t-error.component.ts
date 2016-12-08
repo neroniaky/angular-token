f4c4cfd20e5b3d46f1e1082c19af5a11237ca162
Brian Kosik <brian@Brians-MacBook-Pro.local>
2016-12-08 16:22:57 -0600

    forgot 2 files

--- src/a2t-ui/a2t-shared/a2t-error/a2t-error.component.ts
+++ src/a2t-ui/a2t-shared/a2t-error/a2t-error.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
     selector: 'a2t-error',
     template: '<div *ngFor="let error of errors"><p>{{error}}</p></div>',
-    styleUrls: ['a2t-error.component.css']
+    styleUrls: ['./a2t-error.component.css']
 })
 export class A2tErrorComponent {
 
