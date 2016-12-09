aae4a3afd002de03a33d53660fa2fed03111e5ad
NeroNiaky <neroniaky@gmail.com>
2016-12-09 20:50:50 +0100

    Upgrade testing to webpack 2

--- config/spec-bundle.js
+++ config/spec-bundle.js
@@ -3,18 +3,16 @@ Error.stackTraceLimit = Infinity;
 require('core-js/es6');
 require('core-js/es7/reflect');
 
-// Typescript emit helpers polyfill
 require('ts-helpers');
 
 require('zone.js/dist/zone');
 require('zone.js/dist/long-stack-trace-zone');
-require('zone.js/dist/proxy'); // since zone.js 0.6.15
+require('zone.js/dist/proxy');
 require('zone.js/dist/sync-test');
-require('zone.js/dist/jasmine-patch'); // put here since zone.js 0.6.14
+require('zone.js/dist/jasmine-patch');
 require('zone.js/dist/async-test');
 require('zone.js/dist/fake-async-test');
 
-// RxJS
 require('rxjs/Rx');
 
 var testing = require('@angular/core/testing');
@@ -28,7 +26,7 @@ testing.TestBed.initTestEnvironment(
 var testContext = require.context('../src', true, /\.spec\.ts/);
 
 function requireAll(requireContext) {
-	return requireContext.keys().map(requireContext);
+    return requireContext.keys().map(requireContext);
 }
 
 var modules = requireAll(testContext);
--- config/webpack.test.js
+++ config/webpack.test.js
@@ -1,47 +1,76 @@
-module.exports = {
+const helpers = require('./helpers');
+const path = require('path');
 
-	devtool: 'inline-source-map',
+const ProvidePlugin = require('webpack/lib/ProvidePlugin');
+const DefinePlugin = require('webpack/lib/DefinePlugin');
+const LoaderOptionsPlugin = require('webpack/lib/LoaderOptionsPlugin');
+const ContextReplacementPlugin = require('webpack/lib/ContextReplacementPlugin');
 
-	resolve: {
-		extensions: ['', '.ts', '.js'],
-		root: '../src',
-	},
+module.exports = function (options) {
+    return {
 
-	module: {
-		preLoaders: [{
-			test: /\.js$/,
-			loader: 'source-map-loader',
-			exclude: [
-				'../node_modules/rxjs',
-				'../node_modules/@angular'
-			]
-		}],
-		loaders: [{
-			test: /\.ts$/,
-			loader: 'awesome-typescript-loader',
-			query: {
-				compilerOptions: {
-					removeComments: true
-				}
-			},
-			exclude: [/\.e2e\.ts$/]
-		}],
-		postLoaders: [{
-			test: /\.(js|ts)$/, loader: 'istanbul-instrumenter-loader',
-			include: '../src',
-			exclude: [
-				/\.(e2e|spec)\.ts$/,
-				/node_modules/
-			]
-		}]
-	},
+        devtool: 'inline-source-map',
 
-	node: {
-		global: 'window',
-		process: false,
-		crypto: 'empty',
-		module: false,
-		clearImmediate: false,
-		setImmediate: false
-	}
-};
+        resolve: {
+            extensions: ['.ts', '.js'],
+            modules: [ path.resolve(__dirname, 'src'), 'node_modules' ]
+        },
+
+        module: {
+
+            rules: [
+
+                {
+                    enforce: 'pre',
+                    test: /\.js$/,
+                    loader: 'source-map-loader',
+                    exclude: [
+                        helpers.root('node_modules/rxjs'),
+                        helpers.root('node_modules/@angular')
+                    ]
+                }, {
+                    test: /\.ts$/,
+                    loader: 'awesome-typescript-loader',
+                    query: {
+                        sourceMap: false,
+                        inlineSourceMap: true,
+                        compilerOptions: {
+                        removeComments: true
+                        }
+                    },
+                    exclude: [/\.e2e\.ts$/]
+                }, {
+                    enforce: 'post',
+                    test: /\.(js|ts)$/,
+                    loader: 'istanbul-instrumenter-loader',
+                    include: helpers.root('src'),
+                    exclude: [
+                        /\.(e2e|spec)\.ts$/,
+                        /node_modules/
+                    ]
+                }
+            ]
+        },
+
+        plugins: [
+
+            new ContextReplacementPlugin(
+                    /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+                    helpers.root('src')
+                ),
+                new LoaderOptionsPlugin({
+                debug: true,
+                options: { }
+            }),
+        ],
+
+        node: {
+            global: true,
+            process: false,
+            crypto: 'empty',
+            module: false,
+            clearImmediate: false,
+            setImmediate: false
+        }
+    };
+}
