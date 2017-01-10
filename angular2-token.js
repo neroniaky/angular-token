31d59302c029b2ecca2550f871d6d075354e77ba
NeroNiaky <neroniaky@gmail.com>
2017-01-10 21:04:10 +0100

    Use compiled .js, fixes #103

--- angular2-token.js
+++ angular2-token.js
@@ -2,5 +2,6 @@
 function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
-__export(require('./src/angular2-token.service'));
-__export(require('./src/a2t-ui/a2t-ui.module'));
+__export(require('./lib/angular2-token.service'));
+__export(require('./lib/angular2-token.model'));
+__export(require('./lib/a2t-ui/a2t-ui.module'));
