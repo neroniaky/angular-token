f732dda24b99aeb3e733b077f3347ebdd19f9d8b
NeroNiaky <neroniaky@gmail.com>
2017-06-19 14:28:07 +0200

    update dependencies for angular 4.2

--- src/angular2-token.spec.ts
+++ src/angular2-token.spec.ts
@@ -1,7 +1,7 @@
 import { Http, BaseRequestOptions, Response, ResponseOptions, Headers, RequestMethod } from '@angular/http';
 import { MockBackend } from '@angular/http/testing';
 import { inject, TestBed } from '@angular/core/testing';
-import { ActivatedRoute, RouterOutletMap, RouterState, Router } from '@angular/router';
+import { ActivatedRoute, RouterState, Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { Angular2TokenService } from './angular2-token.service';
