5cf2ff5dfd0645cc939f4cf78c331ebaef30f6c5
NeroNiaky <neroniaky@gmail.com>
2018-01-03 20:42:12 +0100

    update dependencies

--- angular2-token.js
+++ angular2-token.js
@@ -2,6 +2,6 @@
 function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
-__export(require('./lib/angular2-token.service'));
-__export(require('./lib/angular2-token.model'));
-__export(require('./lib/a2t-ui/a2t-ui.module'));
+__export(require('./lib/src/angular2-token.service'));
+__export(require('./lib/src/angular2-token.model'));
+__export(require('./lib/src/a2t-ui/a2t-ui.module'));
