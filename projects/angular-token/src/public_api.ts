c19c241bdbb89c30364bbc82ec9062be5decc3b5
Ryan Morlok <ryan.morlok@morlok.com>
2018-08-16 09:58:23 -0500

    Allow ANGULAR_TOKEN_OPTIONS to be used (#461)

--- projects/angular-token/src/public_api.ts
+++ projects/angular-token/src/public_api.ts
@@ -11,6 +11,8 @@ export {
   AngularTokenOptions
 } from './lib/angular-token.model';
 
+export { ANGULAR_TOKEN_OPTIONS } from './lib/angular-token.token';
+
 export { AngularTokenService } from './lib/angular-token.service';
 
 export { AngularTokenModule } from './lib/angular-token.module';
